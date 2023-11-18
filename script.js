da9f76d6ecd08c8d1a6b9019780b8b9b3b91794a
Five <58424065+fivespects@users.noreply.github.com>
2023-11-18 23:17:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-    const textToCopy = 'denissocorodan@proton.me'; // Change this to the text you want to copy
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
+const secondDigit = document.querySelector('.bgnumber .second');
 
-    // Create a textarea element to hold the text temporarily
-    const textArea = document.createElement('textarea');
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
+let currentSectionIndex = 0;
 
-    // Select the text inside the textarea
-    textArea.select();
-    document.execCommand('copy'); // Copy the selected text to the clipboard
+function updateNumber(newIndex) {
+    const newNumber = `${newIndex + 1}`.padStart(2);
+    const currentNumber = newNumber[1];
 
-    document.body.removeChild(textArea); // Remove the textarea
+    secondDigit.textContent = currentNumber;
+    currentSectionIndex = newIndex;
+}
 
-    // Notify the user
-    alert('Text copied to clipboard: ' + textToCopy);
+function checkSectionInView() {
+    sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+            updateNumber(index);
+            navLinks.forEach(link => link.classList.remove('active'));
+            navLinks[index].classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', () => {
+    checkSectionInView();
+});
+
+window.addEventListener('load', () => {
+    checkSectionInView();
+});
+
+navLinks.forEach((link, index) => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const sectionId = link.getAttribute('href');
+        const section = document.querySelector(sectionId);
+
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+            navLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+            updateNumber(index);
+        }
+    });
 });
